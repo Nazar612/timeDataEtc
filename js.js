3ccb33d0bcdcebc19562903e6ae5d81d81a91203
Nazar612 <nazarstovba300@gmail.com>
2026-09-13 19:15:04 +0200

    3

--- js.js
+++ js.js
@@ -12,4 +12,24 @@ console.log(date.toLocaleTimeString()
 );
 
 const commencerPromotion = new Date("2026-09-19T19:00:00");
-console.log(commencerJsp - date); 
+const totalTimeToPromo = commencerPromotion - date;
+
+function startTimer(difference) {
+    const totalSeconds = Math.floor(difference / 1000);
+
+    const days = Math.floor(totalSeconds / 86400);
+    const ostatok = Math.floor(totalSeconds % 86400)
+    const hours = Math.floor(ostatok / 3600);
+    const ostatok2 = Math.floor(totalSeconds % 3600)
+    const minutes = Math.floor(ostatok2 / 60);
+    const secondes = Math.floor(ostatok2 % 60);
+
+    console.log(days + ":" + hours + ":" + minutes + ":" + secondes);
+}
+
+setInterval(()=> {
+    const date = new Date();
+    const totalTimeToPromo = commencerPromotion - date;
+    startTimer(totalTimeToPromo)
+    // console.log(totalTimeToPromo);
+},1000)
